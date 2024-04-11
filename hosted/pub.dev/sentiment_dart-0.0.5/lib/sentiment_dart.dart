6b846ecc73e545064ccaf63bab56b8dd20f4ae0b
veronikabertels <veronika.bertels@akquinet.de>
2024-04-11 13:33:29 +0200

    v1.19.0

--- hosted/pub.dev/sentiment_dart-0.0.5/lib/sentiment_dart.dart
+++
@@ -1,4 +0,0 @@
-library sentiment_dart;
-
-export 'src/sentiment_dart_base.dart';
-export 'src/sentiment_result.dart';
--- hosted/pub.dev/tor_detector_web-1.1.0/lib/tor_detector_web.dart
+++
@@ -1,8 +0,0 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library tor_detector_web;
-
-export 'src/tor_detector_web_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
