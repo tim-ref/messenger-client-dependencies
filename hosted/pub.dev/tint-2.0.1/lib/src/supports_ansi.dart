8170a8aa12f051b1d716fb06f5d518ed9aa7fba2
TjardL <47039732+TjardL@users.noreply.github.com>
2024-09-18 14:58:24 +0200

    v1.22.0

--- hosted/pub.dev/tint-2.0.1/lib/src/supports_ansi.dart
+++
@@ -1,4 +0,0 @@
-/// Indicate whether ANSI escape sequences are supported or not.
-///
-/// Will be default to `false` if not in a `vm` or `browser` environment.
-bool get supportsAnsiColor => false;
--- hosted/pub.dev/tint-2.0.1/lib/src/supports_ansi_web.dart
+++
@@ -1,5 +0,0 @@
-/// Indicate whether ANSI escape sequences are supported or not.
-///
-/// ANSI escapes will be supported by the browsers
-/// to be able to use in the console.
-bool get supportsAnsiColor => true;
